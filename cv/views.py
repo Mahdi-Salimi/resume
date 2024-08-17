d7d199f776207d094488aa310bf1f32216547ec2
Mahdi-S97 <mahdisalimi.dev@gmail.com>
2024-08-17 12:30:31 +0330

    feat: add context to home and about

--- cv/views.py
+++ cv/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
+
 def home_view(request):
-    return render(request, 'index.html')
+    context = get_about_context()
+    return render(request, 'index.html', context)
 
 def service_detail_view(request):
     return render(request, 'service-details.html')
@@ -13,9 +15,7 @@ def contact_view(request):
     return render(request, 'contact.html')
 
 def about_view(request):
-    context = {
-        'name': 'Mahdi'
-    }
+    context = get_about_context()
     return render(request, 'about.html', context)
 
 def portfolios_view(request):
@@ -23,3 +23,11 @@ def portfolios_view(request):
 
 def services_view(request):
     return render(request, 'services.html')
+
+def get_about_context():
+    return {
+        'name': 'Mahdi',
+        'profile': 'Backend Developer',
+        'email': 'example@gmail.com',
+        'phone': '01234567890'
+    }
