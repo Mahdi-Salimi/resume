a840f9717ae95b07874a6e52ebfac1c7c5d9c811
Mahdi-S97 <mahdisalimi.dev@gmail.com>
2024-08-17 10:51:46 +0330

    feat: create urls and views for service detail and portfolio detail templates

--- cv/urls.py
+++ cv/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import home_view
+from .views import home_view, service_detail_view, portfolio_detail_view
 
 urlpatterns = [
-    path('', home_view, name='home')
+    path('', home_view, name='home'),
+    path('service_detail', service_detail_view, name='service-detail'),
+    path('portfolio_detail', portfolio_detail_view, name='porfolio-detail')
 ]
--- cv/views.py
+++ cv/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request, 'index.html')
+
+def service_detail_view(request):
+    return render(request, 'service-details.html')
+
+def portfolio_detail_view(request):
+    return render(request, 'portfolio-details.html')
